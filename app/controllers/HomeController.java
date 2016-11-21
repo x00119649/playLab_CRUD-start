ec4c4c9752dce22fdfd2d550d00590feb527ef16
Enda Lee <enda.lee@ittdublin.ie>
2016-11-21 17:08:15 +0000

    First commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,16 +4,28 @@ import views.html.*;
 
 public class HomeController extends Controller {
 
-    public Result index(String name) {
-        return ok(index.render("Welcome to the Home page", name));
+    public Result index() {
+        return ok("Welcome to the Home page");
+    }
+    public Result home() {
+        return ok(home.render());
     }
 
-    public Result about() {
-        return ok(about.render());
+    public Result equipment() {
+        return ok(equipment.render());
     }
 
     public Result products() {
         return ok(products.render());
+    }
+    public Result jerseys() {
+        return ok(jerseys.render());
+    }
+    public Result mens() {
+        return ok(mens.render());
+    }
+    public Result womens() {
+        return ok(womens.render());
     }
 
 }
